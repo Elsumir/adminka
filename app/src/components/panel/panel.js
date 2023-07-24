6168933c3202e6ba029c1e052576c957ad6a9639
Serik <askaroff_serik@mail.ru>
2023-07-24 16:49:36 +0300

    fix: при нажатом ctrl, нельзя редактировать

--- app/src/components/panel/panel.js
+++ app/src/components/panel/panel.js
@@ -1,15 +1,41 @@
 import React from 'react';
+console.log(document.querySelector('iframe'));
 
 const Panel = () => {
-    return (
-        <div className="panel">
-            <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-open">Открыть</button>
-            <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-save">Опубликовать</button>
-            <button className="uk-button uk-button-primary uk-margin-small-right" uk-toggle="target: #modal-meta">Редактировать META</button>
-            <button className="uk-button uk-button-default uk-margin-small-right" uk-toggle="target: #modal-backup">Восстановить</button>
-            <button className="uk-button uk-button-danger" uk-toggle="target: #modal-logout">Выход</button>
-        </div>
-    )
+  return (
+    <div className="panel">
+      <button
+        className="uk-button uk-button-primary uk-margin-small-right"
+        uk-toggle="target: #modal-open"
+      >
+        Открыть
+      </button>
+      <button
+        className="uk-button uk-button-primary uk-margin-small-right"
+        uk-toggle="target: #modal-save"
+      >
+        Опубликовать
+      </button>
+      <button
+        className="uk-button uk-button-primary uk-margin-small-right"
+        uk-toggle="target: #modal-meta"
+      >
+        Редактировать META
+      </button>
+      <button
+        className="uk-button uk-button-default uk-margin-small-right"
+        uk-toggle="target: #modal-backup"
+      >
+        Восстановить
+      </button>
+      <button
+        className="uk-button uk-button-danger"
+        uk-toggle="target: #modal-logout"
+      >
+        Выход
+      </button>
+    </div>
+  );
 };
 
 export default Panel;
